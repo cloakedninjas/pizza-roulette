83428355cd5629c3de63897183c651b51249ef50
Daniel Jackson <daniel@cloakedninjas.co.uk>
2019-05-29 21:49:16 +0100

    Basic pizza

---
+++ pizza.js
@@ -0,0 +1,85 @@
+const PI_2 = Math.PI * 2;
+
+class Pizza {
+  constructor(el) {
+    this.el = el;
+    this.ctx = el.getContext('2d');
+
+    // radius in pixels
+    this.radius = 200;
+    this.pepSize = this.radius * 0.15;
+    this.pepMinDist = this.radius * 0.4;
+    this.pepMaxDist = this.radius * 0.8;
+    this.strokeStyle = '#000';
+    this.lineWidth = this.radius * 0.02;
+
+    const padding = 50;
+    this.el.width = (this.radius * 2) + padding;
+    this.el.height = (this.radius * 2) + padding;
+
+    this.names = [
+      'Tinky Winky',
+      'Dipsy',
+      'La La',
+      'Po',
+      'John McClane',
+      'J R R Tolkien'
+    ];
+
+    this.sliceCount = this.names.length;
+    this.slices = [];
+    this.arcSize = PI_2 / this.sliceCount;
+
+    for (let i = 0; i < this.sliceCount; i++) {
+      this.drawSlice(i);
+    }
+  }
+
+  drawSlice(index) {
+    const arcStart = this.arcSize * index;
+    const arcEnd = this.arcSize * (index + 1);
+
+    const ctx = this.ctx;
+
+    ctx.fillStyle = '#ffe192';
+    ctx.strokeStyle = this.strokeStyle;
+    ctx.lineWidth = this.lineWidth;
+
+    // base
+    ctx.beginPath();
+    ctx.arc(this.radius, this.radius, this.radius, arcStart, arcEnd);
+
+    ctx.lineTo(this.radius, this.radius);
+    ctx.fill();
+    ctx.stroke();
+
+
+    // crust
+    const crustSize = this.radius * 0.1;
+    ctx.strokeStyle = '#ff6f36';
+    ctx.lineWidth = crustSize;
+
+    ctx.beginPath();
+    ctx.arc(this.radius, this.radius, this.radius - (crustSize / 2), arcStart, arcEnd);
+    ctx.stroke();
+
+    // pepporoni
+
+    const midArc = arcEnd - (this.arcSize / 2);
+
+    const x = this.radius + Math.cos(midArc) * this.randBetween(this.pepMinDist, this.pepMaxDist);
+    const y = this.radius + Math.sin(midArc) * this.randBetween(this.pepMinDist, this.pepMaxDist);
+
+    ctx.beginPath();
+    ctx.ellipse(x, y, this.pepSize, this.pepSize, 0, 0, PI_2);
+    ctx.fillStyle = '#ff5a5a';
+    ctx.strokeStyle = this.strokeStyle;
+    ctx.lineWidth = this.lineWidth;
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  randBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
